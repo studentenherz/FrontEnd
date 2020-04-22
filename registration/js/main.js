e11f2682dac1ee18f4a9eeb9748e46c8497fe1e2
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-04-22 02:22:30 -0300

    js added

--- registration/js/main.js
+++ registration/js/main.js
@@ -1,9 +1,30 @@
 window.onload = () => {
   var url = window.location.search.substring(1);
-  var data = new Object();
+
+  var get = new Object();
   url.split('&').forEach((item, i) => {
     tmp = item.split('=');
-    data[tmp[0]] = tmp[1];
+    get[tmp[0]] = tmp[1];
   });
-  console.log(data);
+
+  if(get['status'] != undefined){
+    var log = document.getElementsByClassName("log")[0];
+    log.style.display = "block";
+
+    if(get['status'] == "success"){
+      log.innerHTML = "You are successfully " + (log.id == "login-log" ? "logged in!" : "signed up!");
+      log.style.color = "var(--success-color)";
+    }else if((get['status'] == "error"){
+      log.style.color = "var(--alert-color)";
+      if(get['error'] == "empty"){
+        log.innerHTML = "You must fill all the fields!";
+      }else if (get['error'] == "incorrect") {
+        log.innerHTML = "Username or password incorrect!";
+      }      
+    }
+    else{
+      log.innerHTML = "Unknown error :(";
+    }
+
+  }
 }
