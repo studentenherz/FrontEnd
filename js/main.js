37df1c7608aee7ce6f6e3a1514c53c650f521d28
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-04-23 03:12:23 -0300

    logout donde

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
-window.onload = () => {
-  var url = window.location.search.substring(1);
-  var data = new Object();
-  url.split('&').forEach((item, i) => {
-    tmp = item.split('=');
-    data[tmp[0]] = tmp[1];
+function loging() {
+  var logoutBtn = document.getElementById("logout-button");
+  var loginDiv = document.getElementById("login-div");
+
+  if(localStorage["token"] != undefined){
+    logoutBtn.style.display = "block";
+  }
+  else{
+    loginDiv.style.display = "block";
+  }
+
+  logoutBtn.addEventListener('click', () =>{
+    xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200){
+        localStorage.removeItem("token");
+        localStorage.removeItem("name");
+        localStorage.removeItem("username");
+
+        location.reload();
+      }
+    };
+
+    xhttp.open("POST", "../BackEnd/includes/logout.php", true);
+    payload = {
+      logout: true,
+      token: localStorage["token"]
+    }
+    xhttp.setRequestHeader("Content-Type",  "application/json");
+    xhttp.send(JSON.stringify(payload));
+
   });
-  console.log(data);
+
 }
+
+loging()
