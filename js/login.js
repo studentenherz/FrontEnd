f6978470e665080a7c322d86319b462691674afa
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-04-26 01:52:46 -0300

    modal

---
+++ js/login.js
@@ -0,0 +1,44 @@
+var get = new Object();
+
+window.onload = () => {
+  var url = window.location.search.substring(1);
+
+
+  url.split('&').forEach((item, i) => {
+    tmp = item.split('=');
+    get[tmp[0]] = tmp[1];
+  });
+
+  if(get['login'] != undefined){
+    var log = document.getElementsByClassName("log")[0];
+    log.style.display = "block";
+
+    if(get['login'] == "success")
+    {
+      log.innerHTML = "You are successfully " + (log.id == "login-log" ? "logged in!" : "signed up!");
+      log.style.color = "var(--success-color)";
+      document.getElementsByTagName("form")[0].style.display = "none";
+
+      localStorage.setItem('name', get['name']);
+      localStorage.setItem('username', get['username']);
+      localStorage.setItem('token', get['token']);
+
+      setTimeout(()=>{window.location.assign("index.html")}, 2000);
+    }
+    else if(get['login'] == "error")
+    {
+      log.style.color = "var(--alert-color)";
+      if(get['error'] == "empty")
+      {
+        log.innerHTML = "You must fill all the fields!";
+      }else if (get['error'] == "incorrect")
+      {
+        log.innerHTML = "Username or password incorrect!";
+      }
+    }
+    else{
+      log.innerHTML = "Unknown error :(";
+    }
+
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,37 +1,55 @@
-function loging() {
-  var logoutBtn = document.getElementById("logout-button");
-  var loginDiv = document.getElementById("login-div");
+// function loging() {
+//   var logoutBtn = document.getElementById("logout-button");
+//   var loginDiv = document.getElementById("login-div");
+//
+//   if(localStorage["token"] != undefined){
+//     logoutBtn.style.display = "block";
+//   }
+//   else{
+//     loginDiv.style.display = "block";
+//   }
+//
+//   logoutBtn.addEventListener('click', () =>{
+//     xhttp = new XMLHttpRequest();
+//
+//     xhttp.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200){
+//         localStorage.removeItem("token");
+//         localStorage.removeItem("name");
+//         localStorage.removeItem("username");
+//
+//         location.reload();
+//       }
+//     };
+//
+//     xhttp.open("POST", "../BackEnd/includes/logout.php", true);
+//     payload = {
+//       logout: true,
+//       token: localStorage["token"]
+//     }
+//     xhttp.setRequestHeader("Content-Type",  "application/json");
+//     xhttp.send(JSON.stringify(payload));
+//
+//   });
+//
+// }
 
-  if(localStorage["token"] != undefined){
-    logoutBtn.style.display = "block";
+var modal = document.getElementById('login-modal');
+
+window.onclick = function(event){
+  if(event.target == modal){
+    modal.style.display = "none";
   }
-  else{
-    loginDiv.style.display = "block";
-  }
+};
 
-  logoutBtn.addEventListener('click', () =>{
-    xhttp = new XMLHttpRequest();
+// loging()
 
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200){
-        localStorage.removeItem("token");
-        localStorage.removeItem("name");
-        localStorage.removeItem("username");
-
-        location.reload();
-      }
-    };
-
-    xhttp.open("POST", "../BackEnd/includes/logout.php", true);
-    payload = {
-      logout: true,
-      token: localStorage["token"]
-    }
-    xhttp.setRequestHeader("Content-Type",  "application/json");
-    xhttp.send(JSON.stringify(payload));
-
-  });
-
+function login() {
+  document.getElementById('signup-modal').style.display='none';
+  document.getElementById('login-modal').style.display='block';
 }
 
-loging()
+function signup() {
+  document.getElementById('login-modal').style.display='none';
+  document.getElementById('signup-modal').style.display='block';
+}
