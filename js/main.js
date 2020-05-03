25a6c4389ef4fe5d5b366ddcbcd7e62a91618f67
Michel Romero Rodríguez <michelcenrr@gmail.com>
2020-05-02 23:31:56 -0300

    fixed addEventListener('click' ) to onclick

--- js/main.js
+++ js/main.js
@@ -110,28 +110,28 @@ filesArray.push(file);
 console.log(filesArray);
 
 shareBtn.onclick = function() {
-  // if (navigator.canShare && navigator.canShare({files: filesArray })) {
-  //   alert("puedes compartir");
-  //   navigator.share({
-  //     files: filesArray,
-  //     title: 'Prueba',
-  //     text: 'It worked!'
-  //   }).then(()=> console.log('Successfully shared.'))
-  //   .catch((error)=> console.log('Unsuccesful sharing', error));
-  // }
-  // else{
-  //   alert(`You're system doesn't support file sharing`);
-  // }
-  alert("sí");
-  if (navigator.share) {
-            navigator.share({
-                    title: "title.value",
-                    text: "jaja",
-                    url: "url.value",
-                })
-                .then(() => console.log('Successful share'))
-                .catch((error) => console.log('Error sharing', error));
-        } else {
-            console.log("Web Share API is not supported in your browser.")
-        }
+  if (navigator.canShare && navigator.canShare({files: filesArray })) {
+    alert("puedes compartir");
+    navigator.share({
+      files: filesArray,
+      title: 'Prueba',
+      text: 'It worked!'
+    }).then(()=> console.log('Successfully shared.'))
+    .catch((error)=> console.log('Unsuccesful sharing', error));
+  }
+  else{
+    alert(`You're system doesn't support file sharing`);
+  }
+  // alert("sí");
+  // if (navigator.share) {
+  //           navigator.share({
+  //                   title: "title.value",
+  //                   text: "jaja",
+  //                   url: "url.value",
+  //               })
+  //               .then(() => console.log('Successful share'))
+  //               .catch((error) => console.log('Error sharing', error));
+  //       } else {
+  //           console.log("Web Share API is not supported in your browser.")
+  //       }
 }
